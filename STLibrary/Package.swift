e4d61f5e91494c647b68d1afed387a4cc7f06d03
xiguagua <rootxigua@live.com>
2026-06-25 10:25:57 +0800

    chore(repo): show STLibrary in Xcode project

--- STLibrary/Package.swift
+++ STLibrary/Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 
 import PackageDescription
 
 let package = Package(
     name: "STLibrary",
     platforms: [
-        .macOS(.v15)
+//        .macOS(.v15),
+        .iOS(.v18)
     ],
     products: [
         .library(
